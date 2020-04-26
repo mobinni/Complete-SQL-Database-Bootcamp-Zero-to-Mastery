fc28c559f1993171e21c32b33abd3f9d03cbd7f6
Mo Binni <mo@binni.io>
2020-04-26 15:46:25 -0400

    Update questions.sql

--- SQL Deep Dive/Comparison Operators/questions.sql
+++ SQL Deep Dive/Comparison Operators/questions.sql
@@ -1,23 +1,19 @@
 -- How many female customers do we have from the state of Oregon (OR) over 20?
--- Result: ?
 /*
 * Write your query here
 */
 
 -- Who over the age of 44 has an income of 100 000 or more?
--- Result: ?
 /*
 * Write your query here
 */
 
 -- Who between the ages of 30 and 50 has an income less than 50 000?
--- Result: ?
 /*
 * Write your query here
 */
 
 -- What is the average income between the ages of 20 and 50?
--- Result: ?
 /*
 * Write your query here
 */
