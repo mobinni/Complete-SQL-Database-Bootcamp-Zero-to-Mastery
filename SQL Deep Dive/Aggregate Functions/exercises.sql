cc96ae24e68c85334ce24cda7dac01c8678232f5
Mo Binni <mo@binni.io>
2020-04-26 16:04:16 -0400

    Update exercises.sql

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -1,1 +1,41 @@
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: Employees
+ */
+-- 
+
+
+-- Question 1: What is the average salary for the company?
+-- Table: Salaries
+
+
+
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: France
+ */
+-- 
+
+-- Question 1: How many towns are there in france?
+-- Table: Towns
+
+
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: World
+ */
+-- 
+
+-- Question 1: How many official languages are there in the world?
+-- Table: countrylanguage
+
+-- Question 2: What is the average life expectancy in the world?
+-- Table: country
+
+-- Question 3: What is the average population for cities in the netherlands?
+-- Table: city
+
 
