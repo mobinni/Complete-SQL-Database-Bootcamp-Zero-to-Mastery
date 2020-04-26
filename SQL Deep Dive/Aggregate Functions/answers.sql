72dc120659b6890f751b15c9ac7b5e6fbbef6cfd
Mo Binni <mo@binni.io>
2020-04-26 16:09:48 -0400

    Update answers.sql

--- SQL Deep Dive/Aggregate Functions/answers.sql
+++ SQL Deep Dive/Aggregate Functions/answers.sql
@@ -1,1 +1,51 @@
 
+--
+/*
+ * What database should I use for these exercises?
+ * Name: Employees
+ */
+-- 
+
+
+-- Question 1: What is the average salary for the company?
+-- Table: Salaries
+-- Result: 63810.744836143706
+select avg(salary) from salaries;
+
+
+--
+/*
+ * What database should I use for these exercises?
+ * Name: France
+ */
+-- 
+
+-- Question 1: How many towns are there in france?
+-- Table: Towns
+-- Result: 36684
+select count(id) from towns;
+
+--
+/*
+ * What database should I use for these exercises?
+ * Name: World
+ */
+-- 
+
+-- Question 1: How many official languages are there in the world?
+-- Table: countrylanguage
+-- Result: 238
+select count(countrycode) from countrylanguage
+where isofficial = true;
+
+-- Question 2: What is the average life expectancy in the world?
+-- Table: country
+-- Result: 66.48603611164265
+select avg(lifeexpectancy) from country;
+
+-- Question 3: What is the average population for cities in the netherlands?
+-- Table: city
+-- Result: 185001
+select AVG(population) from city
+where countrycode = 'NLD';
+
