7138ddaad0538232bc2ac0f7fe57f606eb72655a
Mo Binni <mo@binni.io>
2020-05-04 20:17:42 -0400

    Update questions.sql

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -1,1 +1,15 @@
 
+/*
+* DB: Store
+* Table: Customers
+* Question: 
+* Select people either under 30 or over 50 with an income above 50000
+* that are from either Japan or Australia
+*/
+
+/*
+* DB: Store
+* Table: Orders
+* Question: 
+* What was our total sales in June of 2004 for orders over 100 dollars?
+*/
