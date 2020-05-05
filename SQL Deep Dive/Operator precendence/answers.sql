0f2594ee1c0e5e097fb881bcd0e570ea0169ac9d
Mo Binni <mo@binni.io>
2020-05-04 20:17:19 -0400

    Create answers.sql

---
+++ SQL Deep Dive/Operator precendence/answers.sql
@@ -0,0 +1,23 @@
+/*
+* DB: Store
+* Table: Customers
+* Question: 
+* Select people either under 30 or over 50 with an income above 50000
+* that are from either Japan or Australia
+*/
+
+SELECT firstname, income, age from customers
+WHERE income > 50000 AND (age <= 30 OR age >= 50)
+and (country = 'Japan' OR country = 'Australia')
+
+
+/*
+* DB: Store
+* Table: Orders
+* Question: 
+* What was our total sales in June of 2004 for orders over 100 dollars?
+*/
+
+SELECT SUM(totalamount) from orders
+WHERE (orderdate >= '2004-06-01' AND orderdate <= '2004-06-30') 
+AND totalamount > 100
