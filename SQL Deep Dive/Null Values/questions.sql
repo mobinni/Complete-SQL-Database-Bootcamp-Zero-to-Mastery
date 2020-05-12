0406d3e22f05d0dca1d17305c0864c2692e45fcc
Mo Binni <mo@binni.io>
2020-05-12 09:53:10 -0400

    Update questions.sql

--- SQL Deep Dive/Null Values/questions.sql
+++ SQL Deep Dive/Null Values/questions.sql
@@ -1,1 +1,12 @@
 
+/*
+* DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
+* Question: 
+* Assuming a students minimum age for the class is 15, what is the average age of a student?
+*/
+
+/*
+* DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
+* Question: 
+* Replace all empty first or last names with a default?
+*/
