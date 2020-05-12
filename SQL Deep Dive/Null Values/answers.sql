82586292a7585d1803180964b70c1365b91ad817
Mo Binni <mo@binni.io>
2020-05-12 09:52:49 -0400

    Update answers.sql

--- SQL Deep Dive/Null Values/answers.sql
+++ SQL Deep Dive/Null Values/answers.sql
@@ -1,1 +1,15 @@
+/*
+* DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
+* Question: 
+* Assuming a students minimum age for the class is 15, what is the average age of a student?
+*
+* 30.4000000000000000
+*/
+SELECT avg(coalesce(age, 15)) FROM "Student";
 
+/*
+* DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
+* Question: 
+* Replace all empty first or last names with a default?
+*/
+SELECT id, coalesce(name, 'fallback'), coalesce(name, 'lastName'), age FROM "Student";
