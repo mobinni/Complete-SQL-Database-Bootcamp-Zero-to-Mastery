4d8eed39093131a5e62baf8e5d177aa8338320cd
Mo Binni <mo@binni.io>
2020-05-16 09:57:01 -0400

    Update answers.sql

--- SQL Deep Dive/Date Filtering/answers.sql
+++ SQL Deep Dive/Date Filtering/answers.sql
@@ -1,1 +1,41 @@
+/*
+* DB: Employees
+* Table: employees
+* Question: Get me all the employees above 50, use the appropriate date functions
+*/
 
+SELECT * FROM employees;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many employees where hired in February?
+*/
+
+SELECT * FROM employees;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many employees were born in november?
+*/
+
+SELECT * FROM employees;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Who is the oldest employee?
+*/
+
+SELECT MAX(AGE(birth_date)) FROM employees;
+
+/*
+* DB: Store
+* Table: orders
+* Question: How many orders were made in January 2004?
+*/
+
+SELECT COUNT(orderid)
+FROM orders
+WHERE DATE_TRUNC('month', orderdate) = date '2004-01-01';
