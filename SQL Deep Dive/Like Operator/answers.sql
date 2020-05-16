e2c4c12acacad1e266b280f83eaa6d723567fc44
Mo Binni <mo@binni.io>
2020-05-16 12:06:06 -0400

    Update answers.sql

--- SQL Deep Dive/Like Operator/answers.sql
+++ SQL Deep Dive/Like Operator/answers.sql
@@ -1,1 +1,51 @@
+/*
+* DB: Employees
+* Table: employees
+* Question: Find the age of all employees who's name starts with M.
+* Sample output: https://imgur.com/vXs4093
+*/
 
+SELECT emp_no, first_name, EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees
+WHERE firstName ILIKE 'M%';
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many people's name start with A and end with R?
+* Expected output: 1846
+*/
+
+SELECT count(emp_no) FROM employees
+WHERE first_name ILIKE 'A%R';
+                                                  
+/*
+* DB: Store
+* Table: customers
+* Question: How many people's zipcode have a 2 in it?.
+* Expected output: 4211 
+*/
+
+SELECT count(customerid) FROM customers
+WHERE zip::text LIKE '%2%';
+
+
+/*
+* DB: Store
+* Table: customers
+* Question: How many people's zipcode start with 2 with the 3rd character being a 1.
+* Expected output: 109 
+*/
+
+SELECT count(customerid) FROM customers
+WHERE zip::text LIKE '2_1%';
+
+/*
+* DB: Store
+* Table: customers
+* Question: Which states have phone numbers starting with 302?
+* Replace null values with "No State"                                                  
+* Expected output: https://imgur.com/AVe6G4c
+*/
+
+SELECT coalesce(state, 'No State') as "State" FROM customers
+WHERE phone::text LIKE '302%';
