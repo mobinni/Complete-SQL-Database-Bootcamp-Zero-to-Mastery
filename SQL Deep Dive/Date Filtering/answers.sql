0373399e0aa877aa814e95dd7262eb8ed7e32bef
Mo Binni <mo@binni.io>
2020-05-16 10:19:59 -0400

    Update answers.sql

--- SQL Deep Dive/Date Filtering/answers.sql
+++ SQL Deep Dive/Date Filtering/answers.sql
@@ -1,18 +1,26 @@
 /*
 * DB: Employees
 * Table: employees
-* Question: Get me all the employees above 50, use the appropriate date functions
+* Question: Get me all the employees above 60, use the appropriate date functions
 */
 
-SELECT * FROM employees;
+SELECT AGE(birth_date), * FROM employees
+WHERE (
+   EXTRACT (YEAR FROM AGE(birth_date))
+) > 60 ;
 
+-- alternative
+SELECT count(birth_date) FROM employees
+WHERE birth_date < now() - interval '61 years' -- 61 years before the current date
+            
 /*
 * DB: Employees
 * Table: employees
 * Question: How many employees where hired in February?
 */
 
-SELECT * FROM employees;
+SELECT count(emp_no) FROM employees
+where EXTRACT (MONTH FROM hire_date) = 2;
 
 /*
 * DB: Employees
@@ -20,7 +28,8 @@ SELECT * FROM employees;
 * Question: How many employees were born in november?
 */
 
-SELECT * FROM employees;
+SELECT COUNT(emp_no) FROM employees
+WHERE EXTRACT (MONTH FROM birth_date) = 11;
 
 /*
 * DB: Employees
