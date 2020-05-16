fffb684da3da1c2f2076bb90fa18b42d1153afd5
Mo Binni <mo@binni.io>
2020-05-16 09:54:08 -0400

    Create questions.sql

---
+++ SQL Deep Dive/Date Filtering/questions.sql
@@ -0,0 +1,40 @@
+/*
+* DB: Employees
+* Table: employees
+* Question: Get me all the employees above 50, use the appropriate date functions
+*/
+
+SELECT * FROM employees;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many employees where hired in February?
+*/
+
+SELECT * FROM employees;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many employees were born in november?
+*/
+
+SELECT * FROM employees;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Who is the oldest employee?
+*/
+
+SELECT * FROM employees;
+
+/*
+* DB: Store
+* Table: orders
+* Question: How many orders were made in January 2004?
+*/
+
+SELECT * FROM orders;
+
