ffd8a52365bf598ee9c0ad25218616ff5530fb51
Mo Binni <mo@binni.io>
2020-05-16 12:06:36 -0400

    Create questions.sql

---
+++ SQL Deep Dive/Like Operator/questions.sql
@@ -0,0 +1,45 @@
+/*
+* DB: Employees
+* Table: employees
+* Question: Find the age of all employees who's name starts with M.
+* Sample output: https://imgur.com/vXs4093
+*/
+
+SELECT emp_no, first_name, EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees
+WHERE firstName ILIKE 'M%';
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many people's name start with A and end with R?
+* Expected output: 1846
+*/
+
+SELECT count(emp_no) FROM employees
+WHERE first_name ILIKE 'A%R';
+                                                  
+/*
+* DB: Store
+* Table: customers
+* Question: How many people's zipcode have a 2 in it?.
+* Expected output: 4211 
+*/
+
+
+
+/*
+* DB: Store
+* Table: customers
+* Question: How many people's zipcode start with 2 with the 3rd character being a 1.
+* Expected output: 109 
+*/
+
+
+/*
+* DB: Store
+* Table: customers
+* Question: Which states have phone numbers starting with 302?
+* Replace null values with "No State"                                                  
+* Expected output: https://imgur.com/AVe6G4c
+*/
+
