68e718c77c3e4e16d88c5a148f70ae08100e6bb7
Mo Binni <mo@binni.io>
2020-05-27 19:27:09 -0400

    Update answers.sql

--- SQL Deep Dive/Distinct/answers.sql
+++ SQL Deep Dive/Distinct/answers.sql
@@ -4,7 +4,7 @@
 * Question: What unique titles do we have?
 */
 
-SELECT * FROM employees;
+SELECT DISTINCT title FROM titles;
 
 
 /*
@@ -13,13 +13,16 @@ SELECT * FROM employees;
 * Question: How many unique birth dates are there?
 */
 
-SELECT * FROM employees;
+SELECT COUNT(DISTINCT birth_date)
+from employees;
 
 /*
 * DB: World
 * Table: country
 * Question: Can I get a list of distinct life expectancy ages
+* Make sure there are no nulls and it's ordered
 */
 
-SELECT * FROM world;
-
+SELECT DISTINCT lifeexpectancy FROM country
+WHERE lifeexpectancy IS NOT NULL
+ORDER BY lifeexpectancy;
