04a5b3647131805d414f3a995344343ca97b528b
Mo Binni <mo@binni.io>
2020-05-27 19:15:33 -0400

    Update questions.sql

--- SQL Deep Dive/Distinct/questions.sql
+++ SQL Deep Dive/Distinct/questions.sql
@@ -1,1 +1,39 @@
+/*
+* DB: Employees
+* Table: employees
+* Question: Get me all the employees above 50, use the appropriate date functions
+*/
 
+SELECT * FROM employees;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many employees where hired in February?
+*/
+
+SELECT * FROM employees;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many employees were born in november?
+*/
+
+SELECT * FROM employees;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Who is the oldest employee? (Use the analytical function MAX)
+*/
+
+SELECT * FROM employees;
+
+/*
+* DB: Store
+* Table: orders
+* Question: How many orders were made in January 2004?
+*/
+
+SELECT * FROM orders;
