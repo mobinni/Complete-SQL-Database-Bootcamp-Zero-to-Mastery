0d287752544855809102a21b22927426f1f9a3d1
Mo Binni <mo@binni.io>
2020-05-27 19:39:27 -0400

    Update answers.sql

--- SQL Deep Dive/IN/answers.sql
+++ SQL Deep Dive/IN/answers.sql
@@ -1,1 +1,20 @@
+/*
+* DB: Store
+* Table: orders
+* Question: How many orders were made by customer 7888, 1082, 12808, 9623
+*/
 
+SELECT COUNT(orderid)
+FROM orders
+WHERE customerid IN (7888, 1082, 12808, 9623)
+
+
+/*
+* DB: World
+* Table: city
+* Question: How many cities are in the district of Zuid-Holland, Noord-Brabant and Utrecht?
+*/
+
+SELECT COUNT(id)
+FROM city
+WHERE district IN ('Zuid-Holland', 'Noord-Brabant', 'Utrecht');
