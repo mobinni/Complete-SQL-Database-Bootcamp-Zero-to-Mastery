51e8917d8925c989021f0f46d27ac1a55f6b226f
Mo Binni <mo@binni.io>
2020-05-27 19:41:52 -0400

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -1,1 +1,19 @@
+/*
+* DB: Employees
+* Table: employees
+* Question: Sort employees by first name ascending and last name descending
+*/
 
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Sort employees by age
+*/
+
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Sort employees who's name starts with a "k" by hire_date
+*/
