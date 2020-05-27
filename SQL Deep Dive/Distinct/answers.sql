3b4238a55e598f0122f99ac9fa606510e16d80d3
Mo Binni <mo@binni.io>
2020-05-27 19:24:09 -0400

    Create answers.sql

---
+++ SQL Deep Dive/Distinct/answers.sql
@@ -0,0 +1,25 @@
+/*
+* DB: Employees
+* Table: titles
+* Question: What unique titles do we have?
+*/
+
+SELECT * FROM employees;
+
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many unique birth dates are there?
+*/
+
+SELECT * FROM employees;
+
+/*
+* DB: World
+* Table: country
+* Question: Can I get a list of distinct life expectancy ages
+*/
+
+SELECT * FROM world;
+
