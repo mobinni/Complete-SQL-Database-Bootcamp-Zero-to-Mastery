e1d7b22501fe6b78b5aed87c0b0e85dafb7b08c5
Mo Binni <mo@binni.io>
2020-05-27 19:37:56 -0400

    Update questions.sql

--- SQL Deep Dive/IN/questions.sql
+++ SQL Deep Dive/IN/questions.sql
@@ -1,1 +1,16 @@
+/*
+* DB: Store
+* Table: orders
+* Question: How many orders were made by customer 7888, 1082, 12808, 9623
+*/
 
+SELECT * FROM orders;
+
+
+/*
+* DB: World
+* Table: city
+* Question: How many cities are in the district of Zuid-Holland, Noord-Brabant and Utrecht?
+*/
+
+SELECT * FROM city;
