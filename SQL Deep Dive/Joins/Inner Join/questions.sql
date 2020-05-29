14b37b1999f7dd97cab865fca1332293926dd226
Mo Binni <mo@binni.io>
2020-05-28 23:44:46 -0400

    Update and rename answers.sql to questions.sql

---
+++ SQL Deep Dive/Joins/Inner Join/questions.sql
@@ -0,0 +1,26 @@
+
+/*
+* DB: Store
+* Table: orders
+* Question: Get all orders from customers who live in Ohio (OH), New York (NY) or Oregon (OR) state
+* ordered by orderid
+*/
+
+
+
+/*
+* DB: Store
+* Table: products
+* Question: Show me the inventory for each product
+*/
+
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Show me for each employee which department they work in
+*/
+
+
+
+
