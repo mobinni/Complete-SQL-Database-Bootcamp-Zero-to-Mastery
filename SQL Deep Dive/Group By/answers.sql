814da4c39d0bcbfb2a50bbb2918933bccf4ccbfc
Mo Binni <mo@binni.io>
2020-06-07 10:05:58 -0400

    Update answers.sql

--- SQL Deep Dive/Group By/answers.sql
+++ SQL Deep Dive/Group By/answers.sql
@@ -24,7 +24,7 @@ HAVING count(t.title) > 2
 ORDER BY e.emp_no;
 
 /*
-*  Show me all the people that have had more than 15 salary changes that work in the department development
+*  Show me all the employees that have had more than 15 salary changes that work in the department development
 *  Database: Employees
 */
 
@@ -35,4 +35,16 @@ JOIN dept_emp AS de USING(emp_no)
 WHERE de.dept_no = 'd005'
 GROUP BY e.emp_no
 HAVING count(s.from_date) > 15
+ORDER BY e.emp_no;
+
+/*
+*  Show me all the employees that have worked for multiple departments
+*  Database: Employees
+*/
+
+SELECT e.emp_no, count(de.dept_no) as "worked for # departments"
+FROM employees as e
+JOIN dept_emp AS de USING(emp_no)
+GROUP BY e.emp_no
+HAVING count(de.dept_no) > 1
 ORDER BY e.emp_no;
