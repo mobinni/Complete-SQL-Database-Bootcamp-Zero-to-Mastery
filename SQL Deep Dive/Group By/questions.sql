a7c67dffd0e4c75bd0193cb42917fb5d0892c635
Mo Binni <mo@binni.io>
2020-06-07 10:06:54 -0400

    Create questions.sql

---
+++ SQL Deep Dive/Group By/questions.sql
@@ -0,0 +1,33 @@
+/*
+*  How many people were hired on did we hire on any given hire date?
+*  Database: Employees
+*  Table: Employees
+*/
+
+SELECT e.emp_no
+FROM employees as e
+
+/*
+*  Show me all the employees, hired after 1991, that have had more than 2 titles
+*  Database: Employees
+*/
+
+SELECT e.emp_no
+FROM employees as e
+
+
+/*
+*  Show me all the employees that have had more than 15 salary changes that work in the department development
+*  Database: Employees
+*/
+
+SELECT e.emp_no
+FROM employees as e
+
+/*
+*  Show me all the employees that have worked for multiple departments
+*  Database: Employees
+*/
+
+SELECT e.emp_no
+FROM employees as e
