bb0fdff64d813047ec1e9f2bc95c90ef5805ff14
Mo Binni <mo@binni.io>
2020-09-15 15:10:15 -0400

    Update questions.sql

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -4,6 +4,7 @@
 * Table: Customers
 * Question: 
 * Select people either under 30 or over 50 with an income above 50000
+* Include people that are 50
 * that are from either Japan or Australia
 */
 
