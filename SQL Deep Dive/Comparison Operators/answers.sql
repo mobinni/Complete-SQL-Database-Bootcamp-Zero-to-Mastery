7860b6a0018b99913bbcaf6188e687b4f8d6e9ce
Mo Binni <mo@binni.io>
2020-09-15 08:04:18 -0400

    Update answers.sql

--- SQL Deep Dive/Comparison Operators/answers.sql
+++ SQL Deep Dive/Comparison Operators/answers.sql
@@ -12,10 +12,10 @@ WHERE age > 44 and income >= 100000;
 
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
+-- Result: 2362
 SELECT COUNT(income)
 FROM customers
-WHERE age >= 30 and age =< 50 AND income < 50000;
-
+WHERE age >= 30 and age <= 50 AND income < 50000;
 
 -- What is the average income between the ages of 20 and 50? (Excluding 20 and 50)
 -- Result: 59409.926240780098
