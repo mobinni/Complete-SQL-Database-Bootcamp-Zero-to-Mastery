98ef35b21c1168182a444822f4332b4cd36e0379
Mo Binni <mo@binni.io>
2020-09-16 13:37:57 -0400

    Update answers.sql

--- SQL Deep Dive/Distinct/answers.sql
+++ SQL Deep Dive/Distinct/answers.sql
@@ -20,7 +20,7 @@ from employees;
 * DB: World
 * Table: country
 * Question: Can I get a list of distinct life expectancy ages
-* Make sure there are no nulls and it's ordered
+* Make sure there are no nulls
 */
 
 SELECT DISTINCT lifeexpectancy FROM country
