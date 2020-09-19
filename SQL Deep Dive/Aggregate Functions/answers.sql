f663c709d12a4f38e99ba9d4fc1e559853453219
Mo Binni <mo@binni.io>
2020-09-19 07:26:40 -0400

    Update answers.sql

--- SQL Deep Dive/Aggregate Functions/answers.sql
+++ SQL Deep Dive/Aggregate Functions/answers.sql
@@ -36,7 +36,7 @@ select count(id) from towns;
  */
 -- 
 
--- Question 1: How many countries have an official language?
+-- Question 1: How many official languages are there?
 -- Table: countrylanguage
 -- Result: 238
 select count(countrycode) from countrylanguage
