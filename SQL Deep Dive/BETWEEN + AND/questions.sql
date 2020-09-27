2c6fd468cf4d24bb10e46d4ee13e4f9b504a8995
Mo Binni <mo@binni.io>
2020-09-27 15:02:16 -0400

    Create questions.sql

---
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -0,0 +1,11 @@
+-- Who between the ages of 30 and 50 has an income less than 50 000?
+-- (include 30 and 50 in the results)
+
+/*
+* Write your query here
+*/
+
+-- What is the average income between the ages of 20 and 50? (Including 20 and 50)
+/*
+* Write your query here
+*/
