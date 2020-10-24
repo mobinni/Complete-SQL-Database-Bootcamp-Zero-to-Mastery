496c227fb3f6500948b88594094ee801e29fb12a
Mo Binni <mo@binni.io>
2020-10-24 10:30:05 -0400

    Update answers.sql

--- SQL Deep Dive/NULLIF/answers.sql
+++ SQL Deep Dive/NULLIF/answers.sql
@@ -1,1 +1,9 @@
+/*
+* DB: Store
+* Table: products
+* Question: Show NULL when the product is not on special (0)
+*/
+
+SELECT prod_id, title, price, NULLIF(special, 0) as "special"
+FROM products
 
