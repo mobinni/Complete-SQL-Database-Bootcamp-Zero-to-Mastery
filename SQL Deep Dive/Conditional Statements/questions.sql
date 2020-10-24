8e4c36f24eeced67f98b804c63c723cbb37dd7b8
Mo Binni <mo@binni.io>
2020-10-24 10:40:57 -0400

    Create questions.sql

---
+++ SQL Deep Dive/Conditional Statements/questions.sql
@@ -0,0 +1,7 @@
+/**
+* Database: Store
+* Table: products
+* Create a case statement that's named "price class" where if a product is over 20 dollars you show 'expensive'
+* if it's between 10 and 20 you show 'average' 
+* and of is lower than or equal to 10 you show 'cheap'.
+*/
