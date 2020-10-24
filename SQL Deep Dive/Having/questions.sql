4e5db95bb6b6a976395087ec15054948b4a4e184
Mo Binni <mo@binni.io>
2020-10-24 09:58:34 -0400

    Create questions.sql

---
+++ SQL Deep Dive/Having/questions.sql
@@ -0,0 +1,20 @@
+
+/*
+*  Show me all the employees, hired after 1991, that have had more than 2 titles
+*  Database: Employees
+*/
+
+
+
+/*
+*  Show me all the employees that have had more than 15 salary changes that work in the department development
+*  Database: Employees
+*/
+
+
+
+/*
+*  Show me all the employees that have worked for multiple departments
+*  Database: Employees
+*/
+
