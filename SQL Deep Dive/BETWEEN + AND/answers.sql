6d5438770dbe553e0ed7177a9c9bdbd91e31f8ec
Mo Binni <mo@binni.io>
2020-11-09 23:51:17 -0500

    Update answers.sql

--- SQL Deep Dive/BETWEEN + AND/answers.sql
+++ SQL Deep Dive/BETWEEN + AND/answers.sql
@@ -1,6 +1,6 @@
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
-SELECT COUNT(income)
+SELECT *
 FROM customers
 WHERE age BETWEEN 30 AND 50 AND income < 50000;
 
