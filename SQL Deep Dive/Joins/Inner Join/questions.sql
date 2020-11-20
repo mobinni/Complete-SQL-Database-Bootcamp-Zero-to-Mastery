d3838114c14c83d39c1f6a158fa7bc810e336f2a
Hanna Mulugeta <Hannabmulugeta@gmail.com>
2020-11-19 23:41:25 -0500

    Update questions.sql
    Selecting data's from Multiple tables using INNER JOIN

--- SQL Deep Dive/Joins/Inner Join/questions.sql
+++ SQL Deep Dive/Joins/Inner Join/questions.sql
@@ -5,7 +5,11 @@
 * Question: Get all orders from customers who live in Ohio (OH), New York (NY) or Oregon (OR) state
 * ordered by orderid
 */
-
+SELECT a.orderid, b.customerid, b.state
+FROM orders AS a 
+INNER JOIN customers AS b ON a.customerid = b.customerid
+AND b.state IN ('OH', 'NY', 'OR')
+ORDER BY orderid;
 
 
 /*
@@ -14,6 +18,9 @@
 * Question: Show me the inventory for each product
 */
 
+SELECT a.prod_id, b.title, a.quan_in_stock
+FROM inventory AS a 
+INNER JOIN products AS b ON a.prod_id = b.prod_id
 
 /*
 * DB: Employees
@@ -21,6 +28,9 @@
 * Question: Show me for each employee which department they work in
 */
 
-
-
+SELECT a.emp_no, concat(a.first_name, ' ' ,a.last_name) AS "Name", c.dept_name 
+FROM employees AS a 
+INNER JOIN dept_emp AS b ON a.emp_no = b. emp_no
+INNER JOIN departments AS c ON b.dept_no = c.dept_no
+ORDER BY emp_no; 
 
