6230dc9a1560b7db28bf4f73d376a0f7646d1249
Hanna Mulugeta <Hannabmulugeta@gmail.com>
2020-11-20 18:10:37 -0500

    Update questions.sql
    Filtering GROUPBY using HAVING keyword.

--- SQL Deep Dive/Having/questions.sql
+++ SQL Deep Dive/Having/questions.sql
@@ -4,17 +4,34 @@
 *  Database: Employees
 */
 
-
+SELECT COUNT (b.title) AS " No of titles", a.emp_no, concat(a.first_name, ' ', a.last_name) AS "Name"
+FROM employees AS a
+JOIN titles AS b USING (emp_no)
+WHERE extract(YEAR FROM hire_date ) > 1991
+GROUP BY a.emp_no
+HAVING COOUNT(b.title)>2
+ORDER BY a.emp_no;
 
 /*
 *  Show me all the employees that have had more than 15 salary changes that work in the department development
 *  Database: Employees
 */
 
-
+SELECT a.emp_no, COUNT(b.salary)AS "Salary Changes"
+FROM employees AS a JOIN salaries AS b using (emp_no)
+JOIN dept_emp AS c using (emp_no)
+WHERE dept_no = 'd005'
+GROUP BY a.emp_no
+HAVING COUNT('Salary Changes') > 15 
+ORDER BY emp_no;
 
 /*
 *  Show me all the employees that have worked for multiple departments
 *  Database: Employees
 */
 
+SELECT a.emp_no, COUNT(b.dept_no) AS "No of Dept woked"
+FROM employees AS a JOIN dept_emp AS b using (emp_no)
+GROUP BY a.emp_no
+HAVING COUNT(b.dept_No) > 1
+ORDER BY a.emp_no;
