20428d9b339179505263ab1dbed1545c40c05c7e
Nicholas Blexrud <nick@blexrud.com>
2021-10-15 12:48:46 -0500

    Update answers.sql

--- SQL Deep Dive/Aggregate Functions/answers.sql
+++ SQL Deep Dive/Aggregate Functions/answers.sql
@@ -14,6 +14,7 @@ select avg(salary) from salaries;
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
+-- Result: 1965-02-01
 select max(birth_date) from employees;
 
 
