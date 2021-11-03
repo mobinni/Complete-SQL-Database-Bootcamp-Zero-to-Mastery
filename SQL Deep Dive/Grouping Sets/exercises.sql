60c3d0b330fdccc7c7d5ac771d86a41c02162ff2
Mo Binni <mo@binni.io>
2021-11-03 14:47:29 -0400

    Update exercises.sql

--- SQL Deep Dive/Grouping Sets/exercises.sql
+++ SQL Deep Dive/Grouping Sets/exercises.sql
@@ -1,5 +1,11 @@
 /*
-*  Calculate the total amount of employees per department using grouping sets
+*  Calculate the total amount of employees per department and the total using grouping sets
+*  Database: Employees
+*  Table: Employees
+*/
+
+/*
+*  Calculate the total average salary per department and the total using grouping sets
 *  Database: Employees
 *  Table: Employees
 */
