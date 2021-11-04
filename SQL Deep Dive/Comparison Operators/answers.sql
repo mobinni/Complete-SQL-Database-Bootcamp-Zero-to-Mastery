c1c649e9639259aa40d94cf18b509d0fa294b1bf
terrifricker <terrifricker@yahoo.com>
2021-11-04 14:44:04 -0700

    Update the questions repeated in answers

--- SQL Deep Dive/Comparison Operators/answers.sql
+++ SQL Deep Dive/Comparison Operators/answers.sql
@@ -4,20 +4,20 @@ SELECT COUNT(firstName)
 FROM customers
 WHERE gender = 'F' and state = 'OR';
 
--- Who over the age of 44 has an income of 100 000 or more? (excluding 44)
+-- How many customers over the age of 44 have an income of 100 000 or more? (excluding 44)?
 -- Result: 2497
 SELECT COUNT(income)
 FROM customers
 WHERE age > 44 and income >= 100000;
 
--- Who between the ages of 30 and 50 has an income less than 50 000?
+-- How many customers between the ages of 30 and 50 have an income less than 50 000?
 -- (include 30 and 50 in the results)
 -- Result: 2362
 SELECT COUNT(income)
 FROM customers
 WHERE age >= 30 and age <= 50 AND income < 50000;
 
--- What is the average income between the ages of 20 and 50? (Excluding 20 and 50)
+-- What is the average income of customers between the ages of 20 and 50? (Excluding 20 and 50)?
 -- Result: 59409.926240780098
 SELECT AVG(income)
 FROM customers
