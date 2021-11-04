24d1b9243df52ffb4368edbaea32a58c36bce27f
terrifricker <terrifricker@yahoo.com>
2021-11-04 15:06:02 -0700

    Remove unneeded question marks that I added to answers file

--- SQL Deep Dive/Comparison Operators/answers.sql
+++ SQL Deep Dive/Comparison Operators/answers.sql
@@ -4,7 +4,7 @@ SELECT COUNT(firstName)
 FROM customers
 WHERE gender = 'F' and state = 'OR';
 
--- How many customers over the age of 44 have an income of 100 000 or more? (excluding 44)?
+-- How many customers over the age of 44 have an income of 100 000 or more? (excluding 44)
 -- Result: 2497
 SELECT COUNT(income)
 FROM customers
@@ -17,7 +17,7 @@ SELECT COUNT(income)
 FROM customers
 WHERE age >= 30 and age <= 50 AND income < 50000;
 
--- What is the average income of customers between the ages of 20 and 50? (Excluding 20 and 50)?
+-- What is the average income of customers between the ages of 20 and 50? (Excluding 20 and 50)
 -- Result: 59409.926240780098
 SELECT AVG(income)
 FROM customers
