8e705d8ba387d311517062d2212c732f0cd4ad79
Nick McGlynn <63480889+notalatte@users.noreply.github.com>
2022-02-28 12:04:46 -0500

    Update questions.sql
    - add database and table signifiers for the questions

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -1,3 +1,5 @@
+-- DB: Store
+-- Table: customers
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
 
@@ -5,7 +7,10 @@
 * Write your query here
 */
 
+-- DB: Store
+-- Table: customers
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
+
 /*
 * Write your query here
 */
