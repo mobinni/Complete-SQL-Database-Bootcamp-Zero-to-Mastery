9ec783ec9b5767be251963105cd1d5d5a5effdc6
Fabio Teichmann <76876855+ele-fant@users.noreply.github.com>
2022-05-28 11:21:19 +0300

    Update questions.sql
    my solution

--- SQL Deep Dive/IN/questions.sql
+++ SQL Deep Dive/IN/questions.sql
@@ -4,7 +4,8 @@
 * Question: How many orders were made by customer 7888, 1082, 12808, 9623
 */
 
-SELECT * FROM orders;
+SELECT * FROM orders
+WHERE customerid IN (7888, 1082, 12808, 9623);
 
 
 /*
@@ -13,4 +14,5 @@ SELECT * FROM orders;
 * Question: How many cities are in the district of Zuid-Holland, Noord-Brabant and Utrecht?
 */
 
-SELECT * FROM city;
+SELECT * FROM city
+WHERE district IN ('Zuid-Holland', 'Noord-Brabant', 'Utrecht');
