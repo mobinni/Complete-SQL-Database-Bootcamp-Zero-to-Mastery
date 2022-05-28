777cc1d64e82355427a048f12391d49599449515
Fabio Teichmann <76876855+ele-fant@users.noreply.github.com>
2022-05-28 11:41:30 +0300

    Update questions.sql
    my solution

--- SQL Deep Dive/Like Operator/questions.sql
+++ SQL Deep Dive/Like Operator/questions.sql
@@ -5,7 +5,8 @@
 * Sample output: https://imgur.com/vXs4093
 * Use EXTRACT (YEAR FROM AGE(birth_date)) we will learn about this in later parts of the course
 */
-SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
+SELECT emp_no, first_name, EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees
+WHERE first_name ILIKE 'm%';
 
 
 /*
@@ -14,6 +15,8 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's name start with A and end with R?
 * Expected output: 1846
 */
+SELECT COUNT(emp_no) FROM employees
+WHERE first_name LIKE 'A%r'
 
                                                   
 /*
@@ -22,7 +25,9 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's zipcode have a 2 in it?.
 * Expected output: 4211 
 */
-
+SELECT COUNT(customerid) FROM customers
+-- need to cast numerical to text for LIKE comparisons in PostgreSQL
+WHERE zip::text LIKE '%2%'
 
 
 /*
@@ -31,7 +36,8 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's zipcode start with 2 with the 3rd character being a 1.
 * Expected output: 109 
 */
-
+SELECT COUNT(customerid) FROM customers
+WHERE zip::text LIKE '2_1%'
 
 /*
 * DB: Store
@@ -40,4 +46,6 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Replace null values with "No State"                                                  
 * Expected output: https://imgur.com/AVe6G4c
 */
+SELECT COALESCE(state, 'No State') FROM customers
+WHERE phone::text LIKE '302%'
 
