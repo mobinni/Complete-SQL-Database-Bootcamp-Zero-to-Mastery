1e191395d44c5595fe39d2cc95828821d19fd7f7
Fabio Teichmann <ft@blocksize-capital.com>
2022-05-30 09:45:56 +0300

    my solutions

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,6 +3,8 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
+SELECT * FROM employees
+ORDER BY first_name ASC, last_name DESC;
 
 
 /*
@@ -10,6 +12,8 @@
 * Table: employees
 * Question: Sort employees by age
 */
+SELECT * FROM employees
+ORDER BY AGE(birth_date);
 
 
 /*
@@ -17,3 +21,6 @@
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+SELECT * FROM employees
+WHERE first_name ILIKE 'k%'
+ORDER BY hire_date;
