e1cd91a0d9c33026363a596c9e6fab0016383f8b
Fabio Teichmann <ft@blocksize-capital.com>
2022-05-31 11:09:08 +0300

    my solutions

--- SQL Deep Dive/Grouping Sets/exercises.sql
+++ SQL Deep Dive/Grouping Sets/exercises.sql
@@ -3,10 +3,30 @@
 *  Database: Employees
 *  Table: Employees
 */
+SELECT d.dept_name, COUNT(e.emp_no)
+FROM departments as d
+INNER JOIN dept_emp as de USING(dept_no)
+INNER JOIN employees as e USING(emp_no)
+GROUP BY 
+    GROUPING SETS(
+        (d.dept_name),
+        () -- refers to the total
+    )
+ORDER BY d.dept_name
+
 
 /*
 *  Calculate the total average salary per department and the total using grouping sets
 *  Database: Employees
 *  Table: Employees
 */
-
+SELECT d.dept_no, d.dept_name, AVG(s.salary)
+FROM departments as d
+INNER JOIN dept_emp as de USING(dept_no)
+INNER JOIN salaries as s USING(emp_no)
+GROUP BY
+    GROUPING SETS(
+        (d.dept_no, d.dept_name),
+        ()
+    )
+ORDER BY d.dept_no
