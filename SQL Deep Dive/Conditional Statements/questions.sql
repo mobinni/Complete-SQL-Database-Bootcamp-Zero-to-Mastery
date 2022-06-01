620646dd402d958629feb0fb4a28152ada3739f1
Fabio Teichmann <ft@blocksize-capital.com>
2022-06-01 11:02:16 +0300

    my solutions

--- SQL Deep Dive/Conditional Statements/questions.sql
+++ SQL Deep Dive/Conditional Statements/questions.sql
@@ -5,3 +5,13 @@
 * if it's between 10 and 20 you show 'average' 
 * and of is lower than or equal to 10 you show 'cheap'.
 */
+
+SELECT
+    prod_id,
+    title,
+    price,
+    CASE WHEN price > 20.00 THEN 'expensive'
+         WHEN price >= 10.00 THEN 'average'
+         ELSE 'cheap'
+    END AS "price class"
+FROM products;
