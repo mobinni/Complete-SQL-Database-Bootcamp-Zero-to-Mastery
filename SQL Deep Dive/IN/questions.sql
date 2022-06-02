5670a82ffe1fc712837270fb095adfed1d1eed43
Fabio Teichmann <ft@blocksize-capital.com>
2022-06-02 09:32:14 +0300

    update solutions

--- SQL Deep Dive/IN/questions.sql
+++ SQL Deep Dive/IN/questions.sql
@@ -4,7 +4,7 @@
 * Question: How many orders were made by customer 7888, 1082, 12808, 9623
 */
 
-SELECT * FROM orders
+SELECT COUNT(orderid) FROM orders
 WHERE customerid IN (7888, 1082, 12808, 9623);
 
 
@@ -14,5 +14,5 @@ WHERE customerid IN (7888, 1082, 12808, 9623);
 * Question: How many cities are in the district of Zuid-Holland, Noord-Brabant and Utrecht?
 */
 
-SELECT * FROM city
+SELECT COUNT(city) FROM city
 WHERE district IN ('Zuid-Holland', 'Noord-Brabant', 'Utrecht');
