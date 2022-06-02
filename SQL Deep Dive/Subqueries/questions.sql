8e757b1df8b46ad5c5e8bd2bc051d962963d2feb
Fabio Teichmann <ft@blocksize-capital.com>
2022-06-02 11:32:29 +0300

    my solutions

--- SQL Deep Dive/Subqueries/questions.sql
+++ SQL Deep Dive/Subqueries/questions.sql
@@ -5,6 +5,25 @@
 * Question: Get all orders from customers who live in Ohio (OH), New York (NY) or Oregon (OR) state
 * ordered by orderid
 */
+-- with JOIN
+select
+    o.orderid,
+    o.customerid,
+    c.state
+from orders as o
+join customers as c using(customerid)
+where c.state in ('OH', 'NY', 'OR')
+order by o.orderid
+
+-- with SUBQUERY
+select
+    o.orderid,
+    o.customerid,
+    (select c.state from customers as c 
+     where c.customerid = o.customerid and c.state in ('OH', 'NY', 'OR')
+     )
+from orders as o
+order by o.orderid
 
 
 /*
@@ -12,3 +31,30 @@
 * Table: employees
 * Question: Filter employees who have emp_no 110183 as a manager
 */
+-- with JOIN
+select
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    dm.emp_no
+from employees as e
+join dept_emp as de using(emp_no)
+join dept_manager as dm using dept_no
+where dm.emp_no = 110183
+order by e.emp_no
+
+-- with SQ
+select
+    e.emp_no,
+    e.first_name,
+    e.last_name
+from employees as e
+where emp_no in (
+    select emp_no from dept_emp
+    where dept_no in (
+        select dept_no from dept_manager
+        where emp_no = 110183
+        )
+    )
+order by e.emp_no
+
