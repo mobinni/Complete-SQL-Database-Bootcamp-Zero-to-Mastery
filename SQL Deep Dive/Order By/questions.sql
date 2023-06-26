bfa15823d22f4b14db275f2dc982e8bfb621c41e
Kyler Jacobs <135051719+KylerJacobs@users.noreply.github.com>
2023-06-26 09:58:07 -0700

    Update questions.sql
    answered questions

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -4,16 +4,24 @@
 * Question: Sort employees by first name ascending and last name descending
 */
 
+select first_name, last_name from "public"."employees"
+order by first_name asc, last_name desc
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees by age
 */
-
+  
+select * from "public"."employees"
+order by birth_date
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+
+select * from "public"."employees"
+where first_name ilike 'k%'
+order by hire_date
