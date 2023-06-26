c95c71a5fb9c05ae2db3935b0b74482a8d7f7992
Kyler Jacobs <135051719+KylerJacobs@users.noreply.github.com>
2023-06-26 13:58:49 -0700

    Update questions.sql
    answered questions

--- SQL Deep Dive/Joins/Inner Join/questions.sql
+++ SQL Deep Dive/Joins/Inner Join/questions.sql
@@ -6,6 +6,10 @@
 * ordered by orderid
 */
 
+select c.firstname, c.lastname, o.orderid from "public"."orders" as o
+inner join "public"."customers" as c on o.customerid = c.customerid
+where c.state in ('OH', 'NY', 'OR')
+order by o.orderid
 
 
 /*
@@ -14,13 +18,19 @@
 * Question: Show me the inventory for each product
 */
 
+select p.prod_id, inventory.quan_in_stock
+from "public"."products" as p
+inner join "public"."inventory" as i on p.prod_id = i.prod_id
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Show me for each employee which department they work in
 */
-
+select e.first_name, dp.dept_name
+from "public"."employees" as e
+inner join "public"."dept_emp" as de on de.emp_no = e.emp_no
+inner join "public"."departments" as dp on dp.dept_no = de.dept_no
 
 
 
