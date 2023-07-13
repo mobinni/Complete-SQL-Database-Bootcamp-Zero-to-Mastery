dbcbd5e52268fd12bb24a68a1c357c3654095d05
BokangMolaoa <126236777+BokangMolaoa@users.noreply.github.com>
2023-07-13 11:12:12 +0200

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -3,9 +3,16 @@
 
 /*
 * Write your query here
+SELECT income, age
+FROM salary
+WHERE income < 50000
+age BETWEEN 30 AND 50;
 */
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
 * Write your query here
+SELECT AVERAGE income 
+FROM salary 
+WHERE age BETWEEN 20 AND 50; 
 */
