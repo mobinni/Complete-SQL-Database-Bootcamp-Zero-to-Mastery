2f248cf90149bb7bf2358d569edef18b3b356505
Vimal Vinayraj <vimalvinayraj@gmail.com>
2023-07-27 00:56:13 +0400

    LIKE

--- SQL Deep Dive/IN/questions.sql
+++ SQL Deep Dive/IN/questions.sql
@@ -1,16 +1,16 @@
 /*
-* DB: Store
-* Table: orders
-* Question: How many orders were made by customer 7888, 1082, 12808, 9623
-*/
-
-SELECT * FROM orders;
-
-
+ * DB: Store
+ * Table: orders
+ * Question: How many orders were made by customer 7888, 1082, 12808, 9623
+ */
+SELECT *
+FROM "public"."orders"
+WHERE customerid IN ('7888', '1082', '12808', '9623');
 /*
-* DB: World
-* Table: city
-* Question: How many cities are in the district of Zuid-Holland, Noord-Brabant and Utrecht?
-*/
-
-SELECT * FROM city;
+ * DB: World
+ * Table: city
+ * Question: How many cities are in the district of "Zuid-Holland", "Noord-Brabant" and "Utrecht"?
+ */
+SELECT *
+FROM "public"."city"
+WHERE district IN ('Zuid-Holland', 'Noord-Brabant', 'Utrecht');
--- SQL Deep Dive/Like Operator/questions.sql
+++ SQL Deep Dive/Like Operator/questions.sql
@@ -1,43 +1,53 @@
 /*
-* DB: Employees
-* Table: employees
-* Question: Find the age of all employees who's name starts with M.
-* Sample output: https://imgur.com/vXs4093
-* Use EXTRACT (YEAR FROM AGE(birth_date)) we will learn about this in later parts of the course
-*/
-SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
-
-
+ * DB: Employees
+ * Table: employees
+ * Question: Find the age of all employees who's name starts with M.
+ * Sample output: https://imgur.com/vXs4093
+ * Use EXTRACT (YEAR FROM AGE(birth_date)) we will learn about this in later parts of the course
+ */
+SELECT...,
+    EXTRACT (
+        YEAR
+        FROM AGE(birth_date)
+    ) as "age"
+FROM employees;
+SELECT *
+FROM "public"."employees"
+WHERE first_name LIKE 'M%';
 /*
-* DB: Employees
-* Table: employees
-* Question: How many people's name start with A and end with R?
-* Expected output: 1846
-*/
-
-                                                  
+ * DB: Employees
+ * Table: employees
+ * Question: How many people's name start with A and end with R?
+ * Expected output: 1846
+ */
+SELECT first_name
+FROM "public"."employees"
+WHERE first_name ILIKE 'A%R';
 /*
-* DB: Store
-* Table: customers
-* Question: How many people's zipcode have a 2 in it?.
-* Expected output: 4211 
-*/
-
-
-
-/*
-* DB: Store
-* Table: customers
-* Question: How many people's zipcode start with 2 with the 3rd character being a 1.
-* Expected output: 109 
-*/
-
-
-/*
-* DB: Store
-* Table: customers
-* Question: Which states have phone numbers starting with 302?
-* Replace null values with "No State"                                                  
-* Expected output: https://imgur.com/AVe6G4c
-*/
-
+ * DB: Store
+ * Table: customers
+ * Question: How many people's zipcode have a 2 in it?.
+ * Expected output: 4211 
+ */
+SELECT COUNT(firstname)
+FROM "public"."customers"
+WHERE zip::text LIKE '2_1%'
+    /*
+     * DB: Store
+     * Table: customers
+     * Question: How many people's zipcode start with 2 with the 3rd character being a 1.
+     * Expected output: 109 
+     */
+SELECT COUNT(firstname)
+FROM "public"."customers"
+WHERE zip::text LIKE '2_1%'
+    /*
+     * DB: Store
+     * Table: customers
+     * Question: Which states have phone numbers starting with 302?
+     * Replace null values with "No State"                                                  
+     * Expected output: https://imgur.com/AVe6G4c
+     */
+SELECT COALESCE(state, 'No state') AS STATE
+FROM "public"."customers"
+WHERE phone::Text LIKE '302%';
